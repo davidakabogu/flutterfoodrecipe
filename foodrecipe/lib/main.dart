982fc6358b1f0795724f04f263ff51c5c78985dd
DavidAkabogu <Akabogudavid@gmail.com>
2024-02-05 21:20:37 +0100

    building UI widgets

--- foodrecipe/lib/main.dart
+++ foodrecipe/lib/main.dart
@@ -6,105 +6,273 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Haters Yakpa'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [
+          ButtonSection(),
+        ],
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              HowToSection(description: "How to Make french toast"),
+              ImageSection(image: 'assets/images/image-13.png'),
+              ChefInfoSection(
+                name: 'Roberta Anny',
+                location: 'Bali Indonesia',
+                image: 'assets/images/unsplash_Ij24Uq1sMwM.png',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
 
-  void _incrementCounter() {
+  void _showPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Button Clicked!'),
+          content: const Text('You have clicked the button.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                // Close the dialog
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              _showPopup(context);
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_horiz),
+            onPressed: () {
+              _showPopup(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HowToSection extends StatelessWidget {
+  const HowToSection({super.key, required this.description});
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(children: [
+        Text(description, style: const TextStyle(fontSize: 40)),
+      ]),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: Image.asset(
+            image,
+            width: 400,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+        ),
+        const FavoriteWidget()
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  double _favoriteRating = 4.5;
+  int _favoriteCount = 300;
+
+  void _toggleFavorite() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_isFavorited) {
+        _favoriteRating -= 0.1;
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteRating += 0.1;
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Row(
+      children: [
+        IconButton(
+          onPressed: _toggleFavorite,
+          icon: (_isFavorited
+              ? const Icon(Icons.star)
+              : const Icon(Icons.star_border)),
+          color: Colors.yellow[700],
+          // alignment: Alignment.bottomLeft,
         ),
+        SizedBox(
+          width: 26,
+          child: SizedBox(
+              child: Text(
+            '$_favoriteRating',
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          )),
+        ),
+        SizedBox(
+          width: 128,
+          child: Text('($_favoriteCount Reviews)',
+              style: const TextStyle(
+                  fontWeight: FontWeight.w500, color: Colors.grey)),
+        ),
+      ],
+    );
+  }
+}
+
+class ChefInfoSection extends StatelessWidget {
+  const ChefInfoSection(
+      {super.key,
+      required this.name,
+      required this.location,
+      required this.image});
+
+  final String name;
+  final String location;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(children: [
+            Row(
+              children: [
+                Image.asset(
+                  image,
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        location,
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ]),
+          const FollowButton()
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class FollowButton extends StatefulWidget {
+  const FollowButton({super.key});
+
+  @override
+  State<FollowButton> createState() => _FollowButtonState();
+}
+
+class _FollowButtonState extends State<FollowButton> {
+  bool _isFollowing = false;
+
+  void _toggleFollow() {
+    setState(() {
+      _isFollowing = !_isFollowing;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _toggleFollow,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: _isFollowing ? Colors.green : Colors.red,
+      ),
+      child: Text(
+        _isFollowing ? 'Following' : 'Follow',
+        style: const TextStyle(color: Colors.white),
+      ),
     );
   }
 }
