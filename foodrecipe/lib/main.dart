aedc5071532813e5f1a7025d198224e111ac2ed0
DavidAkabogu <Akabogudavid@gmail.com>
2024-02-07 11:58:16 +0100

    fade in ingredients feature

--- foodrecipe/lib/main.dart
+++ foodrecipe/lib/main.dart
@@ -121,7 +121,7 @@ class HowToSection extends StatelessWidget {
       Padding(
         padding: const EdgeInsets.all(8.0),
         child: Text(description,
-            style: const TextStyle(fontSize: 34, fontWeight: FontWeight.bold)),
+            style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
       ),
     ]);
   }
@@ -361,53 +361,107 @@ class IngredientsSection extends StatelessWidget {
               final weight = ingredientWeights[index];
               final ingredientImage = ingredientImages[ingredient];
 
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(18.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(12.0),
+              return FadeIn(
+                // Delay each item's animation
+                delay: Duration(milliseconds: index * 100),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: ingredientImage != null
+                                    ? Image.asset(
+                                        ingredientImage,
+                                        width: 70,
+                                        height: 70,
+                                      )
+                                    : const Icon(Icons.image),
                               ),
-                              child: ingredientImage != null
-                                  ? Image.asset(
-                                      ingredientImage,
-                                      width: 70,
-                                      height: 70,
-                                    )
-                                  : const Icon(Icons.image),
                             ),
-                          ),
-                          Text(ingredient,
+                            Text(ingredient,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 22)),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(weight,
                               style: const TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 22)),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(weight,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 18,
-                                color: Colors.grey)),
-                      )
-                    ],
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 18,
+                                  color: Colors.grey)),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               );
             }),
       ],
+    );
+  }
+}
+
+class FadeIn extends StatefulWidget {
+  const FadeIn({super.key, required this.child, required this.delay});
+
+  final Widget child;
+  final Duration delay;
+
+  @override
+  State<FadeIn> createState() => _FadeInState();
+}
+
+class _FadeInState extends State<FadeIn> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200), // Adjust animation duration
+      // Adjust reverse animation duration
+      reverseDuration: const Duration(milliseconds: 200),
+    );
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeIn, // Adjust animation curve
+      ),
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animation,
+      child: widget.child,
     );
   }
 }
