b8343f5d7c63330bd51140857ff049843b3729f1
DavidAkabogu <Akabogudavid@gmail.com>
2024-02-27 22:10:46 +0100

    forms and success msg bottom sheets

--- foodrecipe/lib/widgets/appbarbuttons.dart
+++ foodrecipe/lib/widgets/appbarbuttons.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 class ButtonSection extends StatelessWidget {
@@ -124,50 +126,244 @@ class _TransactionBottomSheetState extends State<TransactionBottomSheet> {
   }
 }
 
-class IncomeForm extends StatelessWidget {
+class IncomeForm extends StatefulWidget {
   const IncomeForm({super.key, required this.onSwitchForm});
   final VoidCallback onSwitchForm;
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      child: const Column(
-        children: <Widget>[
-          Text(
-            'Income Form',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          // Add your income form fields here
-        ],
-      ),
-    );
-  }
+  State<IncomeForm> createState() => _IncomeFormState();
 }
 
-class ExpenseForm extends StatelessWidget {
-  const ExpenseForm({super.key, required this.onSwitchForm});
-  final VoidCallback onSwitchForm;
+class _IncomeFormState extends State<IncomeForm> {
+  final _formKey = GlobalKey<FormState>();
+  late String _name;
+  late String _username;
+  late String _password;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(16.0),
-      child: const Column(
-        children: <Widget>[
-          Text(
-            'Expense Form',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Income name'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter name of Income';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _name = value!;
+              },
             ),
-          ),
-          // Add your expenses form fields here
-        ],
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Select Account'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'First Bank';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _username = value!;
+              },
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Transaction date'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter the date';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _password = value!;
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              // onPressed: _submitForm,
+              onPressed: () => {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return const IncomeSuccessfulBottomSheet();
+                    })
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // Do something with the form data, like sending it to a server
+      print('Name: $_name');
+      print('Username: $_username');
+      print('Password: $_password');
+    }
+  }
+}
+
+class IncomeSuccessfulBottomSheet extends StatelessWidget {
+  const IncomeSuccessfulBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 24.0),
+          child: Icon(Icons.check_circle),
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Successful',
+          style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Your income has been successfully added to earnings',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600)),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ExpenseForm extends StatefulWidget {
+  const ExpenseForm({super.key, required this.onSwitchForm});
+  final VoidCallback onSwitchForm;
+
+  @override
+  State<ExpenseForm> createState() => _ExpenseFormState();
+}
+
+class _ExpenseFormState extends State<ExpenseForm> {
+  final _formKey = GlobalKey<FormState>();
+  late String _name;
+  late String _username;
+  late String _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Transaction name'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Description';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _name = value!;
+              },
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Select Category'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Icon';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _username = value!;
+              },
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Transaction date'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter the date';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _password = value!;
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              // onPressed: _submitForm,
+              onPressed: () => {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return const ExpenseSuccessfulBottomSheet();
+                    })
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // Do something with the form data, like sending it to a server
+      print('Name: $_name');
+      print('Username: $_username');
+      print('Password: $_password');
+    }
+  }
+}
+
+class ExpenseSuccessfulBottomSheet extends StatelessWidget {
+  const ExpenseSuccessfulBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 24.0),
+          child: Icon(Icons.check_circle),
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Successful',
+          style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('expense has been successfully added to transactions',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600)),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
